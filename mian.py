68f521aa6b03a138b0be326fd50aae43e10f7f1f
Sky <156252108@qq.com>
2022-10-30 17:19:11 +0800

    main

---
+++ mian.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2022/10/30 5:18 下午
+# @Author  : Kai Zheng
+# @FileName: mian.py
+# @Software: PyCharm
+# @Email: 156252108@qq.com
+
+if __name__ == '__main__':
+    pass
